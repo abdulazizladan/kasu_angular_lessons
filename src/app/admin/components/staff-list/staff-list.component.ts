366f6523c3b35ee361cff50cd06f778b7e24e68c
Abdulaziz Ladan <mac@Ladans-MacBook-Air.local>
2022-11-16 19:03:50 +0100

    task: Edit staff table data

--- src/app/admin/components/staff-list/staff-list.component.ts
+++ src/app/admin/components/staff-list/staff-list.component.ts
@@ -2,24 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddStaffComponent } from '../add-staff/add-staff.component';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface Staff {
+  firstName: string;
+  lastName: string;
+  id: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+const STUDENTS: Staff[] = [
+  {firstName: "aliyu", lastName: "mustapha", id: "kasu/stf/1234"},
+
 ];
 
 @Component({
@@ -28,8 +19,8 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./staff-list.component.scss']
 })
 export class StaffListComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['id', 'firstName', 'lastName'];
+  dataSource = STUDENTS;
 
   constructor( private addStaffDialog: MatDialog) { }
 
