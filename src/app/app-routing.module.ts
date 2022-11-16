4c7ebdde99550c32efb7672732d3ae11ad0a8088
Abdulaziz Ladan <mac@Ladans-MacBook-Air.local>
2022-11-16 19:26:50 +0100

    fix: Fix default route to redirect to login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
     path: '',
     children: [
       {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
         path: 'login',
         component: LoginComponent
       },
